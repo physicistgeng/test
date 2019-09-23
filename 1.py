d5345cc875ffe8be32d48cb9670e9cc13edce8a4
GengJiwei <gengjiwei_pku@outlook.com>
2019-09-23 15:37:57 +0800

    ad

--- 1.py
+++ 1.py
@@ -2,16 +2,18 @@
 # -*- coding: utf-8 -*-
 
 class Student(object):
-    def __init__(self, name, score):
-        self.name = name 
-        self.score = score 
 
-    def print_score(self):
-        print('%s %s' %(self.name, self.score))
+    @property
+    def score(self):
+        return self._score
 
+    @score.setter
+    def score(self, value):
+        if not isinstance(value, int):
+            raise ValueError('score must be an integer!')
+        if value < 0 or value > 100:
+            raise ValueError('score must between 0 ~ 100!')
+        self._score = value
 
-if __name__ == '__main__':
-    bart = Student('gjw', 96)
-    bart.print_score()
-
-
+from datetime import datetime
+datetime.now()
