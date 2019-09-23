c18d8054282c5faf8aa74f82e174ddb37bca1b15
GengJiwei <gengjiwei_pku@outlook.com>
2019-09-23 10:56:20 +0800

    ad

--- 1.py
+++ 1.py
@@ -1,9 +1,17 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-def aa():
-    pass
-    return
-def haha():
-    pass
-    return
+class Student(object):
+    def __init__(self, name, score):
+        self.name = name 
+        self.score = score 
+
+    def print_score(self):
+        print('%s %s' %(self.name, self.score))
+
+
+if __name__ == '__main__':
+    bart = Student('gjw', 96)
+    bart.print_score()
+
+
